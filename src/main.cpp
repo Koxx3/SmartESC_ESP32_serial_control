3f403fb1446d244b1688e8d7d26e8d21f12e1c35
Koxx3 <koxx33@gmail.com>
2021-01-03 12:24:36 +0100

    fix start sequence & add brake (brake not working)

--- src/main.cpp
+++ src/main.cpp
@@ -25,14 +25,18 @@
 #define SERIAL_FRAME_CMD_FAULT_ACK 0x07
 
 #define FRAME_REG_TARGET_MOTOR 0x00
+#define FRAME_REG_STATUS 0x02
 #define FRAME_REG_CONTROL_MODE 0x03
 #define FRAME_REG_SPEED 0x04
 #define FRAME_REG_TORQUE 0x08
 #define FRAME_REG_SPEED_MEESURED 0x1E
 
 #define THROTTLE_TO_TORQUE_FOCTOR 1.5
+#define THROTTLE_TO_BRAKE_FOCTOR 1
 
 #define TIME_SEND 10 // [ms] Sending time interval
+
+#define DELAY_CMD 200
 
 // #define DEBUG_RX                        // [-] Debug received data. Prints all bytes to serial (comment-out to disable)
 
@@ -136,6 +140,92 @@ typedef struct
 #pragma pack(pop)
 SerialFeedbackError feedbackError;
 
+typedef enum
+{
+  ICLWAIT = 12,               /*!< Persistent state, the system is waiting for ICL
+                           deactivation. Is not possible to run the motor if
+                           ICL is active. Until the ICL is active the state is
+                           forced to ICLWAIT, when ICL become inactive the state
+                           is moved to IDLE */
+  IDLE = 0,                   /*!< Persistent state, following state can be IDLE_START
+                           if a start motor command has been given or
+                           IDLE_ALIGNMENT if a start alignment command has been
+                           given */
+  IDLE_ALIGNMENT = 1,         /*!< "Pass-through" state containg the code to be executed
+                           only once after encoder alignment command.
+                           Next states can be ALIGN_CHARGE_BOOT_CAP or
+                           ALIGN_OFFSET_CALIB according the configuration. It
+                           can also be ANY_STOP if a stop motor command has been
+                           given. */
+  ALIGN_CHARGE_BOOT_CAP = 13, /*!< Persistent state where the gate driver boot
+                           capacitors will be charged. Next states will be
+                           ALIGN_OFFSET_CALIB. It can also be ANY_STOP if a stop
+                           motor command has been given. */
+  ALIGN_OFFSET_CALIB = 14,    /*!< Persistent state where the offset of motor currents
+                           measurements will be calibrated. Next state will be
+                           ALIGN_CLEAR. It can also be ANY_STOP if a stop motor
+                           command has been given. */
+  ALIGN_CLEAR = 15,           /*!< "Pass-through" state in which object is cleared and
+                           set for the startup.
+                           Next state will be ALIGNMENT. It can also be ANY_STOP
+                           if a stop motor command has been given. */
+  ALIGNMENT = 2,              /*!< Persistent state in which the encoder are properly
+                           aligned to set mechanical angle, following state can
+                           only be ANY_STOP */
+  IDLE_START = 3,             /*!< "Pass-through" state containg the code to be executed
+                           only once after start motor command.
+                           Next states can be CHARGE_BOOT_CAP or OFFSET_CALIB
+                           according the configuration. It can also be ANY_STOP
+                           if a stop motor command has been given. */
+  CHARGE_BOOT_CAP = 16,       /*!< Persistent state where the gate driver boot
+                           capacitors will be charged. Next states will be
+                           OFFSET_CALIB. It can also be ANY_STOP if a stop motor
+                           command has been given. */
+  OFFSET_CALIB = 17,          /*!< Persistent state where the offset of motor currents
+                           measurements will be calibrated. Next state will be
+                           CLEAR. It can also be ANY_STOP if a stop motor
+                           command has been given. */
+  CLEAR = 18,                 /*!< "Pass-through" state in which object is cleared and
+                           set for the startup.
+                           Next state will be START. It can also be ANY_STOP if
+                           a stop motor command has been given. */
+  START = 4,                  /*!< Persistent state where the motor start-up is intended
+                           to be executed. The following state is normally
+                           SWITCH_OVER or RUN as soon as first validated speed is
+                           detected. Another possible following state is
+                           ANY_STOP if a stop motor command has been executed */
+  SWITCH_OVER = 19,           /**< TBD */
+  START_RUN = 5,              /*!< "Pass-through" state, the code to be executed only
+                           once between START and RUN states itâ€™s intended to be
+                           here executed. Following state is normally  RUN but
+                           it can also be ANY_STOP  if a stop motor command has
+                           been given */
+  RUN = 6,                    /*!< Persistent state with running motor. The following
+                           state is normally ANY_STOP when a stop motor command
+                           has been executed */
+  ANY_STOP = 7,               /*!< "Pass-through" state, the code to be executed only
+                           once between any state and STOP itâ€™s intended to be
+                           here executed. Following state is normally STOP */
+  STOP = 8,                   /*!< Persistent state. Following state is normally
+                           STOP_IDLE as soon as conditions for moving state
+                           machine are detected */
+  STOP_IDLE = 9,              /*!< "Pass-through" state, the code to be executed only
+                           once between STOP and IDLE itâ€™s intended to be here
+                           executed. Following state is normally IDLE */
+  FAULT_NOW = 10,             /*!< Persistent state, the state machine can be moved from
+                           any condition directly to this state by
+                           STM_FaultProcessing method. This method also manage
+                           the passage to the only allowed following state that
+                           is FAULT_OVER */
+  FAULT_OVER = 11,            /*!< Persistent state where the application is intended to
+                          stay when the fault conditions disappeared. Following
+                          state is normally STOP_IDLE, state machine is moved as
+                          soon as the user has acknowledged the fault condition.
+                      */
+  WAIT_STOP_MOTOR = 20
+
+} State_t;
+
 HardwareSerial hwSerCntrl(1);
 
 void displayBuffer(uint8_t *buffer, uint8_t size)
@@ -190,11 +280,20 @@ void SendSpeed(int16_t brake, int16_t throttle)
 
 void SendTorque(int16_t brake, int16_t throttle)
 {
+  int16_t torque = 0;
+
+  if (brake > 0)
+    torque = -brake;
+  else
+    torque = throttle;
+
+  Serial.printf("torque = %02x\n", torque);
+
   // Create command
   regSet16.Frame_start = SERIAL_START_FRAME_DISPLAY_TO_ESC_REG_SET;
   regSet16.Lenght = 3;
   regSet16.Reg = FRAME_REG_TORQUE;
-  regSet16.Value = throttle;
+  regSet16.Value = torque;
 
   uint16_t sum =
       regSet16.Frame_start + regSet16.Lenght //
@@ -245,7 +344,6 @@ void SendMode(uint8_t mode)
   hwSerCntrl.write((uint8_t *)&regSet8, sizeof(regSet8));
 }
 
-
 void GetReg(uint8_t reg)
 {
   // Create command
@@ -280,28 +378,34 @@ void Receive()
   }
   if (nbBytes > 0)
   {
-    Serial.print("received : ");
+    // display frame
+    Serial.print("   received : ");
     for (int i = 0; i < nbBytes; i++)
     {
       Serial.printf("%02x ", receiveBuffer[i]);
     }
     Serial.println();
 
-    if (nbBytes == 3)
+    if (receiveBuffer[0] == 0xff)
     {
-      Serial.println("=> ok");
+      Serial.printf("   => ko (err : %d) !!!!!!!!!!!!!!!!!!!!!!!!!!!!\n", receiveBuffer[2]);
     }
-    else if (nbBytes == 4)
+    else
     {
-      Serial.printf("=> ko (err : %d)\n", receiveBuffer[2]);
+      if (nbBytes == 3)
+      {
+        Serial.println("   => ok / CMD or REG_SET");
+      }
+      else if (nbBytes == 4)
+      {
+        Serial.printf("   => ok / status : %d\n", receiveBuffer[2]);
+      }
+      else if (nbBytes == 7)
+      {
+        memcpy(&speed, &(receiveBuffer[2]), 4);
+        Serial.printf("   => ok / speed : %d\n", speed);
+      }
     }
-    else if (nbBytes == 7)
-    {
-      memcpy(&speed, &(receiveBuffer[2]), 4);
-      Serial.printf("=> ok / speed : %d\n", speed);
-
-    }
-
   }
 }
 
@@ -326,33 +430,66 @@ void loop(void)
 
   if (state == 0)
   {
-
-    Serial.println("send : CMD STOP");
-    SendCmd(SERIAL_FRAME_CMD_STOP);
+    Serial.println("send : GET REG STATUS");
+    GetReg(FRAME_REG_STATUS);
     state++;
   }
   else if (state == 1)
   {
-
-    Serial.println("send : CMD FAULT_ACK");
-    SendCmd(SERIAL_FRAME_CMD_FAULT_ACK);
+    Serial.println("send : CMD STOP");
+    SendCmd(SERIAL_FRAME_CMD_STOP);
     state++;
+
+    delay(DELAY_CMD);
   }
+
   else if (state == 2)
   {
-
-    Serial.println("send : CMD START");
-    SendCmd(SERIAL_FRAME_CMD_START);
+    Serial.println("send : GET REG STATUS");
+    GetReg(FRAME_REG_STATUS);
     state++;
   }
   else if (state == 3)
   {
 
-    Serial.println("send : SET REG CONTROL_MODE");
-    SendControlMode(0);
+    Serial.println("send : CMD FAULT_ACK");
+    SendCmd(SERIAL_FRAME_CMD_FAULT_ACK);
+    state++;
+
+    delay(DELAY_CMD);
+  }
+
+  else if (state == 4)
+  {
+    Serial.println("send : GET REG STATUS");
+    GetReg(FRAME_REG_STATUS);
+    state++;
+
+    delay(DELAY_CMD);
+  }
+  else if (state == 5)
+  {
+
+    Serial.println("send : CMD START");
+    SendCmd(SERIAL_FRAME_CMD_START);
+    state++;
+
+    delay(DELAY_CMD);
+  }
+  else if (state == 6)
+  {
+    Serial.println("send : GET REG STATUS");
+    GetReg(FRAME_REG_STATUS);
     state++;
   }
-  else if (state == 4)
+  else if (state == 7)
+  {
+
+    Serial.println("send : SET REG CONTROL_MODE");
+    SendControlMode(1);
+    state++;
+  }
+  else if (state == 8)
   {
 
     // Compute throttle
@@ -373,29 +510,36 @@ void loop(void)
     if (analogValueBrake < 0)
       analogValueBrake = 0;
 
-    Serial.println("analogValueThrottleRaw = " + (String)analogValueThrottleRaw + " / analogValueThrottleMinCalibRaw = " + (String)analogValueThrottleMinCalibRaw + " / analogValueThrottle = " + (String)analogValueThrottle);
-#if DEBUG
-    Serial.println("analogValueBrakeRaw = " + (String)analogValueBrakeRaw + " / analogValueBrakeMinCalibRaw = " + (String)analogValueBrakeMinCalibRaw + " / analogValueBrake = " + (String)analogValueBrake);
-#endif
+    Serial.println("analogValueThrottleRaw = " + (String)analogValueThrottleRaw + " / analogValueThrottleMinCalibRaw = " + (String)analogValueThrottleMinCalibRaw + " / analogValueThrottle = " + (String)analogValueThrottle + " / analogValueBrakeRaw = " + (String)analogValueBrakeRaw + " / analogValueBrakeMinCalibRaw = " + (String)analogValueBrakeMinCalibRaw + " / analogValueBrake = " + (String)analogValueBrake);
 
     // Send commands
     Serial.println("send : SET REG TORQUE");
-//    SendTorque(analogValueBrake, torque);
-    SendTorque(analogValueBrake, analogValueThrottle * THROTTLE_TO_TORQUE_FOCTOR);
+    //    SendTorque(analogValueBrake, torque);
+    SendTorque(analogValueBrake * THROTTLE_TO_BRAKE_FOCTOR, analogValueThrottle * THROTTLE_TO_TORQUE_FOCTOR);
 
+#if RAMP_ENABLED
 #define RAMP 400
     if (iLoop % RAMP < RAMP / 2)
       torque = iLoop % RAMP;
     else
       torque = (RAMP / 2) - ((iLoop % RAMP) - (RAMP / 2));
+#endif
+
     state++;
   }
-  else if (state == 5)
+  else if (state == 9)
   {
 
-    Serial.println("command to send : GET REG SPEED");
+    Serial.println("send : GET REG STATUS");
+    GetReg(FRAME_REG_STATUS);
+    state++;
+  }
+  else if (state == 10)
+  {
+
+    Serial.println("send : GET REG SPEED");
     GetReg(FRAME_REG_SPEED_MEESURED);
-    state--;
+    state = state - 2;
   }
 
   iLoop++;
